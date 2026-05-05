e51c5e01d5e19ea7f6a24e75534abd10b72870e8
MR211604 <elmastrollxd34@gmail.com>
2026-05-05 00:40:59 -0600

    deps: upgrading min SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.grader"
-        minSdk = 24
+        minSdk = 35
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
